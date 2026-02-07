8f8688a9333ca6aaabe70daacb4fd18b23d1edea
Soo Lee <dltn990930@naver.com>
2026-02-07 18:39:13 +0900

    refactor: 오타 수정을 위해 UseRepository를 UserRepository로 이름을 변경하였습니다.

--- storage/db-core/src/main/kotlin/io/soo/springboot/storage/db/core/UserRepository.kt
+++ storage/db-core/src/main/kotlin/io/soo/springboot/storage/db/core/UserRepository.kt
@@ -3,7 +3,7 @@ package io.soo.springboot.storage.db.core
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 
-interface UseRepository : JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
+interface UserRepository : JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
     fun existsByEmail(email: String): Boolean
     fun existsByPhoneNumber(phoneNumber: String): Boolean
 }
