3f8a883b2581bdc7b4236060f326caa6803b776a
Soo Lee <dltn990930@naver.com>
2026-02-07 17:53:08 +0900

    feat: SignUp에 이메일 및 전화번호 중복 확인을 추가하였습니다.
    가입 방식에서 이메일과 전화번호를 중복 확인이 통합되어 있습니다. 계정 생성이 성공하면 UserAccountEntity를 반환하도록 서비스를 업데이트했습니다.

--- core/core-api/src/main/kotlin/io/soo/springboot/core/domain/LocalAccountService.kt
+++ core/core-api/src/main/kotlin/io/soo/springboot/core/domain/LocalAccountService.kt
@@ -1,31 +1,53 @@
 package io.soo.springboot.core.domain
 
 import io.soo.springboot.core.enums.Gender
+import io.soo.springboot.storage.db.core.UserAccountEntity
 import io.soo.springboot.storage.db.core.UserAccountRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class LocalAccountService (
     private val accountRepository: UserAccountRepository,
 ){
+    @Transactional
     fun signUp(
         email: String,
         password: String,
-        name: String?,
         nickname: String?,
+        name: String?,
+        locale: String,
         gender: Gender,
         phoneNumber: String,
         profileImageUrl: String?,
         thumbnailImageUrl: String?,
         birthyear: String?,
         birthday: String?,
-    ) {
-        // TODO: 이메일 중복 여부 확인
+    ): UserAccountEntity? {
+        if (accountRepository.findByEmail(email) != null) {
+            throw IllegalArgumentException("이미 존재하는 이메일입니다.")
+        }
+        if (accountRepository.findByPhoneNumber(phoneNumber) != null) {
+            throw IllegalArgumentException("이미 존재하는 전화번호입니다.")
+        }
 
-        // TODO: 전화번호 중복 여부 확인
+        // TODO: 비밀번호 암호화 + 자격 증명 테이블 추가로 생성 및 저장 (추후 로그인이랑 연계 예정)
 
-        // TODO: 회원 정보 저장
-
+        return accountRepository.save(
+            UserAccountEntity(
+                email = email,
+                emailVerified = false,
+                phoneNumber = phoneNumber,
+                phoneVerified = false,
+                name = name,
+                nickname = nickname,
+                gender = gender,
+                locale = locale,
+                birthyear = birthyear,
+                birthday = birthday,
+                profileImageUrl = profileImageUrl,
+                thumbnailImageUrl = thumbnailImageUrl,
+            ),
+        )
     }
-
 }
