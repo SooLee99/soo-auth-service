35e7a639047cdeec0f3c60275f31a0bcee1ee0b5
soo <soo_tack_overflow99@naver.com>
2026-02-19 16:09:02 +0900

    refactor: 'RestAccessDeniedHandler'에서 'ApiResponse.error' 호출 단순화

--- core/core-api/src/main/kotlin/io/soo/springboot/core/api/security/handler/RestAccessDeniedHandler.kt
+++ core/core-api/src/main/kotlin/io/soo/springboot/core/api/security/handler/RestAccessDeniedHandler.kt
@@ -29,7 +29,7 @@ class RestAccessDeniedHandler(
 
         objectMapper.writeValue(
             response.outputStream,
-            ApiResponse.Companion.error(
+            ApiResponse.error(
                 type = type,
                 req = request,
                 fields = mapOf("reason" to "ACCESS_DENIED"),
